115989ea19b96b61b4b5bea9f41cb34f6613b0dd
standvpmnt <standvpmnt@gmail.com>
2021-12-12 14:44:14 +0530

    notes for improvement

--- day12/mod.ts
+++ day12/mod.ts
@@ -3,34 +3,50 @@
     Attempted in TS and Deno
 */
 
+// WARNING!!!!!
+// Computationally expensive right now
+// 33s - cahing didn't impact this
+
 main("input-data");
 
 function readFileToArray(filename: string): string[] {
   return Deno.readTextFileSync(filename).split("\n");
 }
 
-function findNextOptions<T>(char: T, data: Array<T[]>): T[][] {
-  const options = [] as T[][];
-  data.forEach((item) => {
-    if (item.includes(char)) options.push(item);
-  });
-  return options;
+function memoizedFindNext() {
+  const cache = {} as { [key: string]: string[][] };
+  return function findNextOptionsInside(
+    char: string,
+    data: Array<string[]>,
+  ): string[][] {
+    if (Object.keys(cache).includes(char)) {
+      return cache[char];
+    } else {
+      const options = [] as string[][];
+      data.forEach((item) => {
+        if (item.includes(char)) options.push(item);
+      });
+      cache[char] = options;
+      return options;
+    }
+  };
 }
 
 function otherEnd<T>(char: T, data: T[]): T[] {
+  const cache = {} as { [key: string]: string[] };
   return data.filter((item) => item !== char);
 }
 
-function checkAllPathsHaveEnded(paths: string[][]): boolean {
-  let haveEnded = true;
-  for (const path of paths) {
-    if (path[path.length - 1] !== "end") {
-      haveEnded = false;
-      break;
-    }
-  }
-  return haveEnded;
-}
+// TODO: instead of using arrays, try to use objects instead
+/*
+    To use objects instead try the following algorithm to see if possible:
+    1. Pick out all the unique characters from the data
+    2. Each character will become the key and their value will be all of the
+    caves to which they can connect
+    3. Now for every cave, finding the options to which that cave can connect
+    will be significantly faster than the current filtering with includes
+    4. Next see the optimum data-structure for handling each path
+*/
 
 /*
     Ideally there would be a data-structure that would perfectly fit this case
@@ -40,22 +56,23 @@ function checkAllPathsHaveEnded(paths: string[][]): boolean {
     Imagine a stack containing all of the possible options
 */
 function main(filename: string) {
+  const start = Date.now();
   let data = readFileToArray(filename).map((item) => item.split("-"));
   const potentialPaths = [] as string[][];
+  const findNextOptions = memoizedFindNext();
   findNextOptions("start", data).forEach((item) => {
     potentialPaths.push(["start", otherEnd("start", item).join("")]);
   });
   // remove start points from data
   data = [...data.filter((item) => !item.includes("start"))];
-  while (!checkAllPathsHaveEnded(potentialPaths)) {
-    const startWhile = Date.now();
+  const finalizedPaths = [];
+  while (potentialPaths.length > 0) {
     const path = potentialPaths.shift();
     if (path) {
       const lastChar = path[path.length - 1];
       if (lastChar !== "end") {
         const copyOfPath = [...path];
         findNextOptions(lastChar, data).forEach((nextOption) => {
-          const startOptionsFiltering = Date.now();
           const nextEnd = otherEnd(lastChar, nextOption).join("");
           if (nextEnd !== nextEnd.toLowerCase()) {
             potentialPaths.push([...copyOfPath, nextEnd]);
@@ -77,18 +94,13 @@ function main(filename: string) {
               potentialPaths.push([...copyOfPath, nextEnd]);
             }
           }
-          console.log(
-            `Took ${
-              (Date.now() - startOptionsFiltering) / 1000
-            }s in findNextOption Loop`,
-          );
         });
       } else {
-        potentialPaths.push(path);
+        finalizedPaths.push(path);
       }
     }
-    console.log(`Took ${(Date.now() - startWhile) / 1000}s in while loop`);
   }
   //   console.log(potentialPaths.map((item) => item.join(",")).join("\n"));
-  console.log(potentialPaths.length);
+  console.log(finalizedPaths.length);
+  console.log(`Total time taken was ${(Date.now() - start) / 1000}s`);
 }
